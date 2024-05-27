6fbc812ad46f69d1213577f2a39bb8d3cf8401bf
Saccharin01 <chousik01@gmail.com>
2024-05-27 16:43:25 +0900

    데이터 서버 오픈 및 req.url 탐지 대응 시작

--- dataBase.js
+++ dataBase.js
@@ -3,3 +3,12 @@ const XMLHttpRequest = require(`xhr2`);
 const xml = new XMLHttpRequest();
 const fs = require(`fs`)
 const htmlTemplate = require(`../../module/module.HTMLtemplate`)
+
+const server = http.createServer((req, res)=>{
+  if(req.method === 'POST'){
+    console.log(req.url)
+  }
+})
+let PORT = 8088
+server.listen(`${PORT}`)
+console.log(`server running on: http://localhost:${PORT}/`)
