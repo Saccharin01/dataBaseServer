dbcde7995a1144fd31092a3adbee7391a243f6e8
Saccharin01 <chousik01@gmail.com>
2024-05-27 14:45:09 +0900

    htmlTemplate implant

--- dataBase.js
+++ dataBase.js
@@ -2,3 +2,4 @@ const http = require(`http`);
 const XMLHttpRequest = require(`xhr2`);
 const xml = new XMLHttpRequest();
 const fs = require(`fs`)
+const htmlTemplate = require(`../../module/module.HTMLtemplate`)
