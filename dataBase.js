3a6e5b9840873a0281b6f4386ff976f9750ece80
Saccharin01 <chousik01@gmail.com>
2024-05-27 21:09:00 +0900

    금일 작업 임시종료

--- dataBase.js
+++ dataBase.js
@@ -10,12 +10,16 @@ const server = http.createServer((req, res)=>{
     console.log(req.url)
     let body = ""
     req.on('data', (chunk)=>{
+      // console.log(chunk)
       body += chunk.toString();
+      console.log(body)
+      console.log(container)
+      // todo submit data가 많아질 경우에 대한 json 폼팩터가 필요하진 않은지?
     });
     req.on('end', ()=>{
 
       // todo 단위테스트 변경 객체 속에 객체 집어넣어서 json으로 밀어내기
-      
+
       // fs.writeFile(`storage/UserId.json`, body, (err)=>{
       //   if(err){
       //     console.log(err)
