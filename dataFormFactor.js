04ad99b82c421450f54e805f9f318229a8940513
Saccharin01 <chousik01@gmail.com>
2024-05-29 21:16:24 +0900

    데이터 폼팩터 제작에 필요한 업무리스트 및 로직 작성

--- dataFormFactor.js
+++ dataFormFactor.js
@@ -26,10 +26,19 @@ const server = http.createServer((req, res)=>{
       //     console.log(err)
       //   }
       // })
+
+      /**
+       * todo json 데이터를 수용할 폼팩터를 제작
+       * * 지금은 빈 문자열(body)에 서비스 서버에서 넘어온 데이터를 온 로딩하고 있는데
+       * * 하나의 탬플릿을 만들어서(html 탬플릿 같은) 서비스 서버에서 넘어온 데이터를 배열 순환 혹은 객체 메서드로 끼워넣어서 json 데이터로 가공 시키고 저장하는 방식
+       * * readdir 방식과 다른 점은 파일을 여러개 만들지 않음. 폼팩터가 하나 있기 때문에 기존 폼팩터에서 하위로 추가되기만 함
+       * * 쉽게 설명하자면 객체 속에 n개의 객체가 생기게 되는 것. 
+       * todo 컨테이너 역할의 객체에는 user의 데이터가 들어갈텐데 그 데이터를 색인할 key값이 필요함
+       * todo 위에서 만든 key 값에 할당되는 value는 폼팩터로 제작된 데이터의 폼을 따라감
+       * todo 해당 과정은 리터럴 작업을 최대한 배제해야 함
+       */
+
       fs.readdir(`storage`,(err, data)=>{
-        // ! caution
-        //! 스토리지 폴더에 파일이 늘지 않음. readdir로 읽어온다고 해도 파일이 생기지 않으면 길이값의 의미가..
-        //? 연산 시키면 되는거 아님?
         try {
           console.log(`check out this on readdir: `,data)
           fs.writeFile(`storage/userId.json`, body, (err,data)=>{
@@ -37,7 +46,6 @@ const server = http.createServer((req, res)=>{
               console.log(`check out this error : `,err)
             }else{
               console.log(`!!!`+data)
-              // * fs로 파일 작성은 문제없이 구동하고 있음. 단위 로직 조정 필요
             }
           })
 
