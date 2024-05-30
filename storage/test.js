476ce48bf1415acbd9484b8baf045343d49b11a0
Saccharin01 <chousik01@gmail.com>
2024-05-30 21:54:47 +0900

    fatal logic error

--- storage/test.js
+++ storage/test.js
@@ -1,6 +1,19 @@
 const table = {
+  
   'user1' : {
-    
+    "textarea":"asd",
+    "pw":"ffffff",
+    "pwConfirm":"123123123",
+    "email":"",
+    "phoneNum":"1231231231231"
+  },
+
+  'user2': {
+    "textarea":"asd",
+    "pw":"ffffff",
+    "pwConfirm":"123123123",
+    "email":"",
+    "phoneNum":"1231231231231"
   }
 
 }
