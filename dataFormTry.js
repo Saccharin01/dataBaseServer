6fe957f14e8e203c34cdf002cf07a6c98703100c
Saccharin01 <chousik01@gmail.com>
2024-05-31 12:43:58 +0900

    unit test Success DataField Created

--- dataFormTry.js
+++ dataFormTry.js
@@ -28,20 +28,25 @@ const server = http.createServer((req, res)=>{
           // console.log(JSON.parse(data))
           // console.log(typeof(JSON.parse(data)))
           // console.log(Array.isArray(JSON.parse(data)))
-          try {
 
             let origindata = JSON.parse(data)
             console.log(origindata)
             console.log(Array.isArray(origindata))
+
             let userinpt = JSON.parse(chunk)
             console.log(userinpt)
             console.log(typeof(userinpt))
+
             origindata.push(userinpt)
             console.log(origindata)
 
-          } catch (error) {
-            
-          }
+            fs.writeFile(`parseData.json`, JSON.stringify(origindata, null, 2), (err)=>{
+              if(err){
+                console.log(err)
+              }else{
+                console.log(`Process Tracking`)
+              }
+            })
 
         }
       })
