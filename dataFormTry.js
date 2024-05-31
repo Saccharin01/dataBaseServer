3dc5dafb2210367885212c7ad8b5d9cc5faa8d5d
Saccharin01 <chousik01@gmail.com>
2024-05-31 12:11:27 +0900

    unit Test Processing.. Try on fsWF

--- dataFormTry.js
+++ dataFormTry.js
@@ -8,12 +8,33 @@ const server = http.createServer((req, res)=>{
   console.log(req.url)
   if(req.url === `/`){
     req.on(`data`,(chunk)=>{
-      console.log(chunk)
       let container = [];
-      let parseData = JSON.parse(chunk);
-      console.log(parseData)
-      container.push(JSON.parse(chunk));
-      console.log(container)
+      
+      fs.readFile(`parseData.json`,(err,data)=>{
+        if(err){ //? 파일이 없다면
+          container.push(JSON.parse(chunk))
+          console.log(container)
+          let userId = JSON.stringify(container, null, 2) 
+          fs.writeFile(`parseData.json`, userId, (err)=>{
+            if(err){
+              console.log(`WF Caution : ${err}`);
+            }
+            else{
+              console.log(`Process Tracking`)
+            }
+          })
+
+
+        }
+      })
+
+      // console.log(chunk)
+      // let container = [];
+      // let parseData = JSON.parse(chunk);
+      // console.log(parseData)
+      // container.push(JSON.parse(chunk));
+      // console.log(container)
+
     });
     // fs.readFile(`parseData.json`,(err,data)=>{
     //   if(err){
