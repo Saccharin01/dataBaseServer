1b51fe084c4d31aac0c866bceb1157ed4f5967b9
Saccharin01 <chousik01@gmail.com>
2024-06-10 17:54:51 +0900

    important logic build

--- constructor.js
+++ constructor.js
@@ -6,19 +6,22 @@ class UserInfo{
     this.PhoneNum = PhoneNum
   }
 }
-
-let test = new UserInfo(1,2,3,4)
+let data = {
+  'email':'',
+'phoneNum':'ㅁㄴㅇㄹ',
+'pw':'ㅁㄴㅇㄹ',
+'pwConfirm':'ㅁㄴㅇㄹ',
+'userId':'ㅁㄴㅇㄹ'
+}
+let test = new UserInfo(data)
 
 console.log(test)
-let object = {
-  'id' : test
-};
 
-let test22 = JSON.stringify(test,null,2)
-console.log(object.test22)
+// let test22 = JSON.stringify(test,null,2)
+// console.log(object.test22)
 
 const fs = require('fs')
-fs.writeFile('testJson.json', test22, 'utf-8', (err)=>{
+fs.writeFile('testJson.json', JSON.stringify(test, null, 2), 'utf-8', (err)=>{
   if(err){
     console.log(`error!!!! : ${err}`)
   }
