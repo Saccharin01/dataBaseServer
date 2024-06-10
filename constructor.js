65ec8ad1ed24ff34526ec91dd79c54c07ab9118b
Saccharin01 <chousik01@gmail.com>
2024-06-10 17:43:24 +0900

    logic check

--- constructor.js
+++ constructor.js
@@ -10,6 +10,16 @@ class UserInfo{
 let test = new UserInfo(1,2,3,4)
 
 console.log(test)
+let object = {
+  'id' : test
+};
 
 let test22 = JSON.stringify(test,null,2)
-console.log(test22)
+console.log(object.test22)
+
+const fs = require('fs')
+fs.writeFile('testJson.json', test22, 'utf-8', (err)=>{
+  if(err){
+    console.log(`error!!!! : ${err}`)
+  }
+})
